433ca832807f148f1845a91d9d91dbd7baa01d9f
Samuel Durante <samueldurante.h@gmail.com>
2025-06-24 21:41:02 +0000

    Add a new field that indicates if a node is available on nodes table

--- migrations/2_nodes.sql
+++ migrations/2_nodes.sql
@@ -1,7 +1,8 @@
 CREATE TABLE nodes
 (
-  public_key text primary unique not null,
-  alias                   unique not null,
+  public_key text primary key    not null,
+  is_enabled boolean             not null,
+  alias      text         unique not null,
   capacity   numeric(16,8)       not null,
   first_seen timestamptz         not null,
   created_at timestamptz         not null default now(),
