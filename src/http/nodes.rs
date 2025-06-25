b4e53789dd4175a91dde89d1df7d697f7d32152e
Samuel Durante <samueldurante.h@gmail.com>
2025-06-25 02:11:14 +0000

    Implement endpoint to return all nodes saved on database

--- src/http/nodes.rs
+++ src/http/nodes.rs
@@ -1,11 +1,32 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, extract::State, routing::get};
+use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
 
-use super::ApiContext;
+use super::{ApiContext, Result};
 
 pub(crate) fn router() -> Router<ApiContext> {
     Router::new().route("/nodes", get(get_nodes))
 }
 
-async fn get_nodes() -> &'static str {
-    "Hello, nodes!"
+#[derive(Deserialize, Serialize)]
+struct Node {
+    public_key: String,
+    alias: String,
+    capacity: Option<f64>,
+    #[serde(with = "time::serde::rfc3339")]
+    first_seen: OffsetDateTime,
+}
+
+async fn get_nodes(state: State<ApiContext>) -> Result<Json<Vec<Node>>> {
+    let nodes = sqlx::query_as!(
+        Node,
+        r#"
+        SELECT public_key, alias, capacity::float8, first_seen
+        FROM nodes
+        "#
+    )
+    .fetch_all(&state.db)
+    .await?;
+
+    Ok(Json(nodes))
 }
